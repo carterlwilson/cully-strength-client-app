74ddfb9ed18d147b0481cde9e9d89485a6685c6e
Carter Wilson <carterlwilson@gmail.com>
2024-01-10 22:18:57 -0600

    changed text

--- components/Exercise.js
+++ components/Exercise.js
@@ -1,13 +1,21 @@
 import React from "react"
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
-export default function ExerciseView(props) {
+const styles = StyleSheet.create({
+    exerciseText: {
+      fontSize: 50
+    }
+});
+
+const ExerciseView = (props) => {
     return (
         <View>
             <Text>Name: {props.ExerciseName}</Text>
-            <Text>Sets: {props.Sets}</Text>
+            <Text style={styles.exerciseText}>Sets: {props.Sets}</Text>
             <Text>Reps: {props.Reps}</Text>
             <Text>Weight: {props.Weight * props.Multiiplier}</Text>
         </View>
     )
 }
+
+export default ExerciseView;
--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -4,6 +4,7 @@ import {Text, View, TextInput, StyleSheet, Alert, Button} from 'react-native';
 import FirestoreStorage from '../libraries/FirestoreStorage';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { TouchableOpacity } from 'react-native-web';
 
 export default function WelcomeScreen(props) {
 
@@ -85,32 +86,34 @@ export default function WelcomeScreen(props) {
         <View style={styles.screen}>
             {(clientUsername != "" && clientId != "") ? (
                 <View>
-                    <Text>Welcome back {clientFirstName}! Select which workout you're doing today.</Text>
-                    <View style={styles.dayButton}>
-                        <Button
-                            color="#228B22"
-                            title='Day 1'
-                            onPress={() => goToDay(0)} />
+                    <Text style={styles.welcomeScreenText}>Welcome back {clientFirstName}! Select which workout you're doing today.</Text>
+                    <View style={{marginBottom: 10}}>
+                        <TouchableOpacity
+                            style={styles.dayButton}
+                            onPress={() => goToDay(0)}>
+                            <Text style={styles.dayButtonText}>DAY 1</Text>
+                        </TouchableOpacity>
                     </View>
-                    <View style={styles.dayButton}>
-                        <Button
-                            color="#228B22"
-                            title='Day 2'
-                            onPress={() => goToDay(1)} />
+                    <View style={{marginBottom: 10}}>
+                        <TouchableOpacity
+                            style={styles.dayButton}
+                            onPress={() => goToDay(1)}>
+                            <Text style={styles.dayButtonText}>DAY 2</Text>
+                        </TouchableOpacity>
                     </View>
-                    <View style={styles.dayButton}>
-                        <Button
-                            color="#228B22"
-                            title='Day 3' 
-                            onPress={() => goToDay(2)}/>
+                    <View style={{marginBottom: 10}}>
+                        <TouchableOpacity
+                            style={styles.dayButton}
+                            onPress={() => goToDay(2)}>
+                            <Text style={styles.dayButtonText}>DAY 3</Text>
+                        </TouchableOpacity>
                     </View>
                     <View style={styles.submitButton}>
-                        <Button
-                            color="#228B22"
-                            title='Switch User'
+                        <TouchableOpacity
+                            style={styles.dayButton}
                             onPress={switchUser}>
-                            <Text style={styles.submitButton}>Switch User</Text>
-                        </Button>
+                            <Text style={styles.dayButtonText}>SWITCH USER</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
             ) : (
@@ -143,7 +146,13 @@ const styles = StyleSheet.create({
       padding: 10,
     },
     dayButton: {
-        marginTop: 10,
+        backgroundColor: "#228B22",
+        alignItems: 'center',
+        padding: 2,
+    },
+    dayButtonText: {
+        fontSize: 20,
+        color: 'white',
     },
     submitButton: {
         marginTop: 10,
@@ -151,5 +160,9 @@ const styles = StyleSheet.create({
     },
     screen: {
         margin: 10
+    },
+    welcomeScreenText: {
+        fontSize: 20,
+        marginBottom: 20
     }
   });
